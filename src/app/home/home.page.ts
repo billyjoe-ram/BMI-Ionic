b93d64e640c464df9008c89559943c690ee805ef
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-10-05 11:58:39 -0300

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
+  
+  async showToast() {
+    var altura = parseFloat((<HTMLInputElement>document.querySelector("np-altura")).value);
+    var peso = parseFloat((<HTMLInputElement>document.querySelector("inp-peso")).value);
+    var imc = peso / (altura * altura);
+
+    const toast = await this.toastController.create({
+      message: "Seu IMC é de" + imc + "kg/m²",
+      duration: 2000
+    });
+    toast.present();
+  }  
 
 }
